c335386c76fbeb1c7b6acce3f01306ec6ebe82ba
ima2132 <ima2132@columbia.edu>
2023-05-19 17:34:22 -0400

    updated weather forecast processing and display/search history; need to secure api key

--- assets/script.js
+++ assets/script.js
@@ -16,13 +16,31 @@ function getWeather(lat, lon, city) {
       .then(response => response.json())
       .then(data => {
         
-        // show current weather forecast 
-        const currentWeather = data.list[0];
-        let weatherIcon = getWeatherIcon(currentWeather.weather[0].main);
-        document.getElementById('current-weather').innerHTML = `
-                  <h2>${city} <i class="${weatherIcon}"></i></h2>
-                  <p>${currentWeather.dt_txt}</p>
-                  <p>Temperature: ${(currentWeather.main.temp - 273.15).toFixed(2)}°C</p>
-                  <p>Humidity: ${currentWeather.main.humidity}%</p>
-                  <p>Wind Speed: ${currentWeather.wind.speed}m/s</p>
-              `;
+ // show current weather forecast 
+    const currentWeather = data.list[0];
+    let weatherIcon = getWeatherIcon(currentWeather.weather[0].main);
+     document.getElementById('current-weather').innerHTML = `
+    <h2>${city} <i class="${weatherIcon}"></i></h2>
+        <p>${currentWeather.dt_txt}</p>
+        <p>Temperature: ${(currentWeather.main.temp - 273.15).toFixed(2)}°C</p>
+        <p>Humidity: ${currentWeather.main.humidity}%</p>
+        <p>Wind Speed: ${currentWeather.wind.speed}m/s</p>`;
+              
+// show 5-day forecast
+const forecast = data.list.filter((_, index) => index % 8 === 0).slice(1);
+document.getElementById('forecast').innerHTML = forecast.map(day => {
+let dayWeatherIcon = getWeatherIcon(day.weather[0].main);
+    return `
+        <div>
+        <h3>${day.dt_txt} <i class="${dayWeatherIcon}"></i></h3>
+        <p>Temperature: ${(day.main.temp - 273.15).toFixed(2)}°C</p>
+        <p>Humidity: ${day.main.humidity}%</p>
+        <p>Wind Speed: ${day.wind.speed}m/s</p>
+        </div>
+        `
+    }).join('');
+
+// add to search history
+addToHistory(city);
+});
+}
